77ec0efd5a4f9b2907ea4f4dd74dc4192af5cb17
R-R-Freitas <rodrigoaz8@gmail.com>
2022-09-20 18:46:50 -0300

    test: create product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,27 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    id = 1
+    nome_do_produto = "Produto frágil"
+    nome_da_empresa = "Fabricante"
+    data_de_fabricacao = "2022-05-11"
+    data_de_validade = "2023-05-11"
+    numero_de_serie = "A01B02"
+    instrucoes_de_armazenamento = "Com muito cuidado!"
+    product = Product(
+        id,
+        nome_do_produto,
+        nome_da_empresa,
+        data_de_fabricacao,
+        data_de_validade,
+        numero_de_serie,
+        instrucoes_de_armazenamento,
+    )
+    assert product.id == id
+    assert product.nome_do_produto == nome_do_produto
+    assert product.nome_da_empresa == nome_da_empresa
+    assert product.data_de_fabricacao == data_de_fabricacao
+    assert product.data_de_validade == data_de_validade
+    assert product.numero_de_serie == numero_de_serie
+    assert product.instrucoes_de_armazenamento == instrucoes_de_armazenamento
