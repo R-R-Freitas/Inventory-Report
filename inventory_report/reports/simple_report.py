205a28aaa542b7cd724412ac6692155b573c960b
R-R-Freitas <rodrigoaz8@gmail.com>
2022-09-20 21:21:26 -0300

    refactor: SimpleReport.generate method

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -2,7 +2,8 @@ from datetime import datetime
 
 
 class SimpleReport:
-    def generate(products):
+    @classmethod
+    def generate(cls, products):
         oldest_date = min(
             [product["data_de_fabricacao"] for product in products]
         )
@@ -13,22 +14,22 @@ class SimpleReport:
                 if product["data_de_validade"] > str(datetime.today().date())
             ]
         )
-
-        def get_company_with_most_products(products):
-            companies = dict()
-            for product in products:
-                if product["nome_da_empresa"] in companies.keys():
-                    companies[product["nome_da_empresa"]] += 1
-                else:
-                    companies[product["nome_da_empresa"]] = 1
-            print(companies)
-            # source: https://stackoverflow.com/questions/268272/
-            # getting-key-with-maximum-value-in-dictionary
-            return max(companies, key=companies.get)
-
-        company_with_most_products = get_company_with_most_products(products)
+        company_with_most_products = cls.get_company_most_products(products)
         return (
             f"Data de fabricação mais antiga: {oldest_date}\n"
             f"Data de validade mais próxima: {closest_date}\n"
             f"Empresa com mais produtos: {company_with_most_products}"
         )
+
+    @classmethod
+    def get_company_most_products(cls, products):
+        companies = dict()
+        for product in products:
+            if product["nome_da_empresa"] in companies.keys():
+                companies[product["nome_da_empresa"]] += 1
+            else:
+                companies[product["nome_da_empresa"]] = 1
+        print(companies)
+        # source: https://stackoverflow.com/questions/268272/
+        # getting-key-with-maximum-value-in-dictionary
+        return max(companies, key=companies.get)
