229a399c31c9a152d8b357a970ad044160498c9c
R-R-Freitas <rodrigoaz8@gmail.com>
2022-09-20 22:11:36 -0300

    feat: CompleteReport Class

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -6,7 +6,6 @@ class CompleteReport(SimpleReport):
     def generate(cls, products):
         simple_report = super().generate(products=products)
         count_companies = super().count_product_per_company(products=products)
-        print(count_companies)
         complete_report = ""
         for key, value in count_companies.items():
             complete_report += f"- {key}: {value}\n"
