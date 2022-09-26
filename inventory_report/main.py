c86c366ba7307049e922eac51ea7f50da63ac67f
R-R-Freitas <rodrigoaz8@gmail.com>
2022-09-25 22:53:26 -0300

    feat: main, working, test error

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,39 @@
+import sys
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+from inventory_report.reports.colored_report import ColoredReport
+
+importers = {
+    "csv": CsvImporter,
+    "son": JsonImporter,
+    "xml": XmlImporter,
+}
+
+report_types = {
+    "completo": CompleteReport,
+    "simples": SimpleReport,
+    "colorido": ColoredReport
+}
+
+
 def main():
-    pass
+    try:
+        path = sys.argv[1]
+        type = sys.argv[2]
+        importer = importers.get(path[-3:])
+        report_type = report_types.get(type)
+        inventory = InventoryRefactor(importer)
+        inventory.import_data(path, type)
+        inventory_iterator = iter(inventory)
+        if report_type == ColoredReport:
+            report = ColoredReport(SimpleReport)
+            colored_report = report.generate(list(inventory_iterator))
+            print(colored_report)
+        else:
+            print(report_type.generate(list(inventory_iterator)))
+    except IndexError:
+        sys.stderr.write("Verifique os argumentos\n")
